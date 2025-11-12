ba025caf9403f69ea39738d0779fb2e72383e38b
IanLasa <ilasallore@educacion.navarra.es>
2025-11-12 18:22:31 +0100

    Update Program.cs

--- WroteAndFound_Launcher/Program.cs
+++ WroteAndFound_Launcher/Program.cs
@@ -2,9 +2,15 @@ namespace WroteAndFound
 {
     class Program
     {
-        // Variables Globales
+        /// Variables Globales
+
+        // Array global para crear menús
         static string[] menu = new string[10];
+
+        // Lista global para los Usuarios
         static List<List<string>> Usuarios = new List<List<string>>();
+
+        // Lista Global para los libros
         static List<List<string>> Libros = new List<List<string>>()
         {
             new List<string>() { "Cien años de soledad", "Gabriel García Márquez", "1967", "Realismo mágico" },
@@ -12,9 +18,14 @@ namespace WroteAndFound
             new List<string>() { "El Principito", "Antoine de Saint-Exupéry", "1943", "Fábula" },
             new List<string>() { "Don Quijote de la Mancha", "Miguel de Cervantes", "1605", "Novela clásica" }
         };
+
+        // Una simple variable global para salir
         static bool salir = false;
+
+        // Una simple variable globas para opciones
         static int opcion;
 
+        // Se ejecura automaticamente cuando se ejecuta el programa
         static void Main(string[] args)
         {
             Console.WriteLine("");
@@ -23,11 +34,9 @@ namespace WroteAndFound
         }
 
 
-        /// 
         /// Complementos para funciones
-        /// 
 
-        // Funcion para comprobar que el número que nos dan es
+        // Funcion para comprobar que el número que nos dan es correcto
         static int pedirNumero(string pregunta, int x, int y, bool especial)
         {
             int numero;
@@ -40,7 +49,7 @@ namespace WroteAndFound
                 // Para casos especiales
                 if (especial && entrada == "")
                 {
-                    return entrada;
+                    return -1;
                 }
 
                 // Comprueba que lo que escriba el usuario se puede convertir en un int.
@@ -98,7 +107,8 @@ namespace WroteAndFound
         // Función para comprobar que hay libros
         static bool hayLibros()
         {
-            if (Libros.Count == 0) {
+            if (Libros.Count == 0)
+            {
                 // Simplemente le pedí a ChatGPT que pusiese los emojis
                 Console.WriteLine("─────────────────────────────────────────────────────────");
                 Console.WriteLine("📭  Actualmente no hay libros en la biblioteca...");
@@ -106,58 +116,67 @@ namespace WroteAndFound
                 Console.WriteLine("");
                 Console.WriteLine("✨  ¿Por qué no vas y añades uno nuevo? 📖");
                 Console.WriteLine("─────────────────────────────────────────────────────────");
-                Console.WriteLine("")
-                Console.WriteLine("Presiona una tecla para volver...")
+                Console.WriteLine("");
+                Console.WriteLine("Presiona una tecla para volver...");
                 Console.ReadKey();
                 return false;
-            } else {
+            }
+            else
+            {
                 return true;
             }
         }
-        
 
+        
+        /// Menu principal
+                
+        // Menu principal e inicio del progrmama
         static void ejecutarPrograma()
         {
+            Console.Clear();
+            verMenu();
             while (!salir)
             {
-                Console.Clear();
-                verMenu();
-                opcion = pedirNumero("Escoge una opción ", 0, 8);
-                switch(opcion)
+                opcion = pedirNumero("Escoge una opción ", 0, 8, false);
+                switch (opcion)
                 {
-                    case 0:
-                        catalogoLibros();
-                        break;
                     case 1:
-                        crearUsuario();
+                        menuCatalogoLibros();
                         break;
                     case 2:
-                        cogerPrestado();
+                        crearUsuario();
                         break;
                     case 3:
-                        reservarLibro();
+                        cogerPrestado();
                         break;
                     case 4:
-                        verCaracteristicas();
+                        reservarLibro();
                         break;
                     case 5:
-                        verPrestamo();
+                        verCaracteristicas();
                         break;
                     case 6:
-                        devolverLibro();
+                        verPrestamo();
                         break;
                     case 7:
-                        ponerResenia();
+                        devolverLibro();
                         break;
                     case 8:
+                        ponerResenia();
+                        break;
+                    case 0:
                         Console.WriteLine("¡Gracias por venir a Wrote&Found! ¡Esperamos verte de nuevo!");
                         salir = true;
                         break;
                 }
             }
         }
+
+        // Menú principal
         static void verMenu()
         {
+            Console.Clear();
+            Console.WriteLine("");
             Console.WriteLine("       --------------------");
             Console.WriteLine("       |                | |");
             Console.WriteLine("       |                | |");
@@ -168,82 +187,135 @@ namespace WroteAndFound
             Console.WriteLine("       |                | |");
             Console.WriteLine("       --------------------");
             Console.WriteLine("");
+            Console.WriteLine("  ¡Bienvenido/a a la biblioteca!");
+            Console.WriteLine("");
+            Console.WriteLine("  Presiona una tecla para continuar");
+            Console.ReadKey();
+
+            Console.Clear();
+
             menu = new string[] { "Catálogo de libros", "Crear un Usuario", "Coger un libro prestado", "Reservar un libro prestado", "Características de un libro", "Mirar préstamos en curso", "Devolver un libro", "Poner una reseña" };
             menuBonito("MENÚ", menu, 37);
         }
 
-        /// OPCIÓN CATÁLOGO DE LIBROS
-        static void catalogoLibros()
-        {
 
-            static void catalogoLibros()
+        /// OPCIÓN CATÁLOGO DE LIBROS
+
+        // Menú catálogo de libros
+        static void menuCatalogoLibros()
+        {
+            do
             {
                 Console.Clear();
-                menu = new string[] { "Ver todos los libros", "Buscar por autor", "Añadir nuevo libro", "Volver al menú" };
+                menu = new string[] { "Ver todos los libros", "Buscar por autor", "Añadir nuevo libro", "Eliminar un libro", "Buscar por género", "Volver al menú" };
                 menuBonito("CATÁLOGO DE LIBROS", menu, 37);
-                int opcion = pedirNumero("Escoge una opción ", 0, 4);
-            }
-          
-            // Función ver libros
-            static void verLibros()
-            {
-                Console.Clear();
-                if (hayLibros)
+                opcion = pedirNumero("Escoge una opción ", 0, 5, false);
+                switch (opcion)
                 {
-                    do
-                    {
-                        Console.WriteLine("");
-                        Console.WriteLine("╔════════════════════════════╗");
-                        for (int i = 0; i < Libros.Count; i++)
-                        {
-                            bool muchosLibros;
-                            Console.WriteLine($"║   📘 {i+1}. {Libros[i][1]}  ║");
-                            if ((i + 1) % 5 = 0)
-                            {
-                                // Por arreglar
-                                Console.WriteLine("║   ... Presiona intro para ver mas libros")
-                                Console.WriteLine("");
-                                muchosLibros = true
-    
-                            } else {
-                                Console.WriteLine("║   ... No hay mas libros")
-                                Console.WriteLine("")
-                                muchosLibros = false;
-                            }
-    
-                            Console.WriteLine("║      0. Salir")
-                            Console.WriteLine("╚════════════════════════════╝")
-                            Console.WriteLine("");
-                            Console.WriteLine("Puedes escribir el número del libro que quieres ver mas...")
-                            Console.WriteLine("")
-                            opcion = pedirNumero("¿Que quieres hacer?", 1, i + 1, muchosLibros)
-                            if (opcion <= 0 && opcion >= (i + 1))
-                            {
-                                verCaracteristicasLibro(opcion);
-                            }
-                        } 
-                    } while (!salir)
+                    case 1:
+                        verLibros();
+                        break;
+                    case 2:
+                        anhadirLibro();
+                        break;
+                    case 3:
+                        eliminarLibro();
+                        break;
+                    case 4:
+                        buscarAutor();
+                        break;
+                    case 5:
+                        buscarGenero();
+                        break;
+                    case 0:
+                        Console.WriteLine("Volviendo al menú principal...");
+                        salir = true;
+                        break;
                 }
-            }
-    
-            // Ver características de los libros
-            static void verCaracteristicasLibro(int l)
+            } while (!salir);     
+        }
+        
+        // Función ver libros
+        static void verLibros()
+        {
+            Console.Clear();
+            if (hayLibros())
             {
-                Console.WriteLine("---------------------------------------")
-                Console.WriteLine($"📘 Libro {i + 1}:");
-                Console.WriteLine($"   Título: {Libros[l][0]}");
-                Console.WriteLine($"   Autor:  {Libros[l][1]}");
-                Console.WriteLine($"   Año:    {Libros[l][2]}");
-                Console.WriteLine($"   Género: {Libros[l][3]}");
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine("Pulsa una tecla para volver")
-                Console.ReadKey();
+                do
+                {
+                    bool muchosLibros;
+                    Console.WriteLine("");
+                    Console.WriteLine("╔════════════════════════════╗");
+                    for (int i = 0; i < Libros.Count; i++)
+                    {
+                        Console.WriteLine($"║   📘 {i + 1}. {Libros[i][1]}  ║");
+                        if ((i + 1) % 5 == 0)
+                        {
+                            // Por arreglar
+                            Console.WriteLine("║   ... Presiona intro para ver mas libros");
+                            Console.WriteLine("");
+                            muchosLibros = true;
+
+                        }
+                    }
+                    Console.WriteLine("║   ... No hay mas libros");
+                    Console.WriteLine("");
+                    muchosLibros = false;
+
+                    Console.WriteLine("║      0. Salir");
+                    Console.WriteLine("╚════════════════════════════╝");
+                    Console.WriteLine("");
+                    Console.WriteLine("Puedes escribir el número del libro que quieres ver mas...");
+                    Console.WriteLine("");
+                    opcion = pedirNumero("¿Que quieres hacer?", 0, Libros.Count, muchosLibros);
+                    if (opcion <= 0 && opcion >= Libros.Count)
+                    {
+                        verCaracteristicasLibro(opcion);
+                    }
+                } while (!salir);
             }
         }
 
-        /// 
+        // Ver características de los libros
+        static void verCaracteristicasLibro(int l)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine($"📘 Libro {l + 1}:");
+            Console.WriteLine($"   Título: {Libros[l][0]}");
+            Console.WriteLine($"   Autor:  {Libros[l][1]}");
+            Console.WriteLine($"   Año:    {Libros[l][2]}");
+            Console.WriteLine($"   Género: {Libros[l][3]}");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Pulsa una tecla para volver");
+            Console.ReadKey();
+        }
+
+        // Función añadir libro
+        static void anhadirLibro()
+        {
+
+        }
+
+        // Función eliminar libro
+        static void eliminarLibro()
+        {
+
+        }
+
+        // Función buscar por autor
+        static void buscarAutor()
+        {
+
+        }
+
+        //static void buscar por género
+        static void buscarGenero()
+        {
+
+        }
+        
         /// OPCIÓN CREAR CUENTA
-        /// 
+        
         static void crearUsuario()
         {
             Console.Clear();
@@ -283,5 +355,4 @@ namespace WroteAndFound
     }
 
 } 
-
 
